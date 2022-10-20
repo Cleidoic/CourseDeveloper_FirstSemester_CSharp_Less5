4c0486c3d840ac53455e30e8a5c03d8e2169a53a
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-10-20 17:44:01 +0300

    Added first task

--- Homework/Task_1/Program.cs
+++ Homework/Task_1/Program.cs
@@ -3,3 +3,48 @@
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+    Console.WriteLine(message);
+    while (!isCorrect)
+    {
+        isCorrect = int.TryParse(Console.ReadLine(), out result);
+        if (!isCorrect)
+        {
+            Console.WriteLine($"Ввели не число или оно слишком большое. Введите корректное число \n");
+        }
+    }
+    return result;
+}
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    Random number = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = number.Next(100, 1000);
+    }
+
+    return array;
+}
+
+void ShowEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++; 
+    }
+    Console.WriteLine("В массиве: [{0}], {1} четных элемента", string.Join(", ", array), count);
+}
+
+ShowEvenNumbers(
+    GetArray(
+        GetNumber("Введите размер массива: ")
+    )
+);
