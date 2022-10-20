d9d1c84375e9bec05799a162a7144f5c41f8e63a
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-10-20 22:50:06 +0300

    Added second task

--- Homework/Task_2/Program.cs
+++ Homework/Task_2/Program.cs
@@ -4,3 +4,49 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+    Console.WriteLine(message);
+    while (!isCorrect)
+    {
+        isCorrect = int.TryParse(Console.ReadLine(), out result);
+        if (!isCorrect)
+        {
+            Console.WriteLine($"Ввели не число или оно слишком большое. Введите корректное число \n");
+        }
+    }
+    return result;
+}
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    Random number = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = number.Next(-1000, 1001);
+    }
+    Console.WriteLine("Source array: [{0}]", string.Join(", ", array));
+    return array;
+}
+
+int UnevenSum(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        result = result + array[i];
+    }
+    Console.WriteLine("Sum uneven elements of array: {0}", result);
+    return result;
+}
+
+
+UnevenSum(
+    GetArray(
+        GetNumber("Enter the size of the array: ")
+    )
+);
